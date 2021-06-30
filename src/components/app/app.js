ccc901c663ec8fbceb887eec400f725818712967
Bershadsky Mihail Sergeevich <Michail.Bershadsky@ilimtimber.corp>
2021-06-30 16:28:59 +0800

    self events

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,32 @@
 import React from 'react';
-
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
+import './app.scss';
 
-import './app.css';
 
 const App = () => {
-    return (
-    <div>
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList/>
-            <PostAddForm/>
+
+    const data = [
+        {label: 'Going to learn React', id:'qwerty'},
+        {label: 'That is good', id:'qwert'},
+        {label: 'I need a break...', id:'qwer'}
+    ];
+return (
+    
+    <div className="app">
+        <AppHeader/>
+        <div className="search-panel d-flex">
+            <SearchPanel/>
+            <PostStatusFilter/>
         </div>
-        
+        <PostList posts={data}/>
+        <PostAddForm/>
     </div>
+        
+    
     )
 }
 export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,12 +2,21 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+    const elements = posts.map(item => {
+        return (
+            <li key={item.id} className="list-group-item">
+                <PostListItem 
+                    label={item.label}
+                    onDelete={() => console.log('Deleted')}
+                    />
+            </li>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 };
